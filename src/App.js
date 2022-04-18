8b60a30d00d47361ea8b4fbd46c0362c5a914488
amit <amitmanore@691gmail.com>
2022-04-18 12:08:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+
+// import './App.css';
+// import Navbar from './components/Navbar';
+import About from './components/About';
+import Alert from './components/Alert';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
+// import React, { useState } from "react";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+  
+// } from "react-router-dom";
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <h1>amit is good boy</h1> */}
+      {/* <Router> */}
+        <Navbar title={`text me`} home=" Ghar" />
+        <Alert fomo="hey there how are you" />
+        <div className=' container my-3'>
+        <TextForm heading='Enter whatever you want' />
+        <About />
+          {/* <Routes> */}
+          {/* <Route exact path="/About" element={<About/>}> */}
+              {/* <About /> */}
+            {/* </Route> */}
+            {/* <Route exact path="/" element={<TextForm/>}> */}
+              {/* <TextForm heading='Enter whatever you want' /> */}
+            {/* </Route> */}
+           {/* </Routes> */}
+        </div>
+      {/* </Router> */}
+
+    </>
+
   );
 }
 
